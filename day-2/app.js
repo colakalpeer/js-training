e69335cf29752a6db48e19c0a72254be88e508b0
Alper Colak <colakalpeer93@gmail.com>
2025-03-18 02:48:38 +0300

    feat: I have added the level 1 questions for the second day

--- day-2/app.js
+++ day-2/app.js
@@ -1,51 +1,69 @@
+/**Day 2: Exercises: Level 1
+ 
+1.Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+2.Print the string on the browser console using console.log()
+
+3.Print the length of the string on the browser console using console.log()
+
+4.Change all the string characters to capital letters using toUpperCase() method
+
+5.Change all the string characters to capital letters using toLowerCase() method
+
+6.Cut(slice) out the first word of the string using substr() or substring() method
+
+7.Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
+8.Check if the string contains a word Script using includes() method
+
+9.Split the string into an array using split() method
+
+10.Split the string 30 Days Of Javascript at the space using split() method
+
+11.'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon, split the string at the comma and change it to an array.
+
+12.Change 30 Days Of Javascript to 30 Days Of Python using replace() method
+
+13.What is character at index 15 in '30 Days Of Javascript' string? Use charAt() method
+
+14.What is the character code of J in '30 Days Of Javascript' string using charCodeAt()
+
+15.Use indexOf to determine the position of the first occurrence of a in 30 Days Of Javascript.
+
+16.Use lasIndexOf to determine the position of the last occurrence of a in 30 Days Of Javascript.
+
+17.Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end
+a sentence with because because because is a conjunction'
+
+18.Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot
+end a sentence with because because because is a conjunction'
+
+19.Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end
+a sentence with because because because is a conjunction'
+
+20.Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g '30 Days Of Javascript'.
+
+21.Use startsWith() method with the string 30 Days Of Javascript and make the result true
+
+22.Use endsWith() method with the string 30 Days Of Javascript and make the result true
+
+23.Use match() nethod to find all the a's in 30 Days Of Javascript
+
+24.Use concat() and merge '30 Days Of' and 'JavaScript' to single string, '30 Days Of Javascript'
+
+25.Use repeat() method to print 30 Days Of Javascript 2 times
+
+
+ */
 
 
 
 
 
+/**
+ Level: 1 Solutions
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
 
 1.
 let challenge = '30 Days Of Javascript';
