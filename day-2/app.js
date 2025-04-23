746b8f8e95bdb93104ae04fbbd188d8934bff437
colakalpeer <colakalpeer93@gmail.com>
2025-04-23 20:42:54 +0300

    feat: day2 exercises solutions done

--- day-2/app.js
+++ day-2/app.js
@@ -183,3 +183,66 @@ The quote 'There is no exercise better for the heart than reaching down and lift
 because because because is a conjunction'
  
  */
+
+
+1.
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+);
+
+2.
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same --with charity you give love, so don't just give money but reach out your hand instead.");
+
+3.
+let num = 10;
+if (typeof num != 10) {
+    num = Number(num);
+}
+console.log(num, typeof num);
+
+4.
+let floatNum = parseFloat('9.8');
+if (floatNum !== 10) {
+    floatNum = Math.ceil(floatNum);
+}
+console.log(floatNum);
+
+5.
+console.log('python'.includes('on') && 'jargon'.includes('on'));
+
+6.
+let sentence2 = "I hope this course is not full of jargon.";
+console.log(sentence2.includes('jargon'));
+
+7.
+console.log(Math.floor(Math.random() * 101));
+
+8.
+console.log(Math.floor(Math.random()* 51 ) + 50);
+
+9.
+console.log(Math.floor(Math.random() * 256));
+
+10.
+let js = 'JavaScript';
+let randIndex = Math.floor(Math.random() * js.length);
+console.log(js[randIndex]);
+
+11.
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
+12.
+let fullSentence = 'You cannot end a sentence with because because because is a conjunction';
+let start = fullSentence.indexOf('because');
+let end = fullSentence.lastIndexOf('because') + 'because'.length;
+let becausePhrase = fullSentence.substr(start, end-start);
+console.log(becausePhrase);
+
+
+
+
+
+
+
+
+
+
