78206f02e6e10798bb824bce3e3ce704bf57cb78
colakalpeer <colakalpeer93@gmail.com>
2025-04-24 19:01:56 +0300

    feat: day2 questions solutions done

--- day-2/app.js
+++ day-2/app.js
@@ -252,4 +252,35 @@ end a sentence with because because because is a conjunction.
 */
 
 
+1.
+let sentence3 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+let count = (sentence3.match(/love/gi) || []).length;
+console.log(count);
 
+2.
+let sentenceBecause = 'You cannot end a sentence with because because because is a conjunction';
+let countBecause = (sentenceBecause.match(/because/g) || []).length;
+console.log(countBecause);
+
+3.
+let messySentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+let cleanSentence = messySentence.replace(/[^a-zA-Z ]/g, " ");
+
+let words = cleanSentence.split(' ');
+let wordCounts = {};
+for(let i = 0; i < words.length; i++) {
+    if(words[i] !== '') {
+        wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1;
+    }
+}
+
+let maxWord = '';
+let maxCount = 0;
+for(let word in wordCounts) {
+    if(wordCounts[word] > maxCount) {
+        maxCount = wordCounts[word];
+        maxWord = word;
+    }
+}
+
+console.log(`Most frequent word is '${maxWord}' with count ${maxCount}`);
