31324de16a7401ba9c0e68019ab5f50ec21089a7
colakalpeer <colakalpeer93@gmail.com>
2025-05-02 09:08:47 +0300

    feat: day3 all exercise questions added

--- day-3/app.js
+++ day-3/app.js
@@ -89,9 +89,84 @@ the result confirm it by using console.log()
 
 
 
+
+
+
+
+
+
+
 /**Day 3: Exercises: Level 2 
  
-1.
+1.Write a script thath prompt the user to enter base and height of the triangle and calculate and are of a triangle (area = 0.5 x b x h).
+
+    Enter base: 20
+    Enter height: 10
+    The area of the triangle is 100
+
+2.Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate 
+the perimeter of triangle (perimeter = a + b + c)
+
+    Enter side a: 5
+    Enter side b: 4
+    Enter side c: 3
+    The are of the triangle is 12
+
+3.Get length and with using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle 
+(perimeter = 2 x length + width))
+
+4.Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle (c = 2 x pi x r) where pi = 3.14
+
+5.Calculate the slope, x-intercept and y-intercept of y = 2x -2
+
+6.Slope is (m = y2-y1/x2-x1). Find the slope between point (2,2) and point (6,10)
+
+7.Compare the slope of above the questions.
+
+8.Calculate the value of y (y = x^2+6x+9). Try to use different x values and figure out at what x value y is 0.
+
+9.Write a script that prompt a user to enter hours and rate per hour. Calculate pay of the person ?
+
+    Enter hours: 40
+    Enter rate per hour: 28
+    Your weekly earn is 1120
+
+10.If the length of your name is greater than 7 say, your name is long else say your name is short.
+
+11.Compare your first name length and your family name length and you should get thıs output.
+
+    let firstName = 'Alper'
+    let lastName = 'Colak'
+
+12.Declare two variables myAge and my yourAge and assign them initial values and myAge and yourAge.
+
+    let myAge = 250
+    let yourAge = 25
+    
+        I am 225 years older than you.
+
+13.Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user
+ to wait a certain amount of years.
+
+    Enter birth year: 1993
+    You are 31. You are old enough to drive
+
+    Enter birth year: 2010
+    You are 15. You will be allowed to drive after 3 years.
+
+14.Write a script that prompt the user to enter number of years. Calculate a number of seconds a person can live.
+Assume some one lives just hundred years.
+
+    Enter number of years you live: 100
+    You lived 3153600000 seconds.
+
+15.Create a human readable time format using the Date time object
+
+    i.   YYYY-MM-DD HH:mm
+    ii.  DD-MM-YYYY HH:mm
+    iii. DD/MM/YYYY HH:mm
+
+ */
 
 
 
@@ -111,6 +186,100 @@ the result confirm it by using console.log()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**Day 3: Exercises: Level 3
+ 
+1.Create a human readable time format using the Date time object. The hour and the minute should be all the time
+two digits (7 hours should be 07 and 5 minutes should be 05)
+
+    i. YYY-MM-DD HH:mm eg. 20120-01-02 07:05
 
 
 
