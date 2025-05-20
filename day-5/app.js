475c361bafaa5de5c479a93ed33ee5db77a91a1c
colakalpeer <colakalpeer93@gmail.com>
2025-05-20 22:12:47 +0300

    fixed: name changed

--- day-5/app.js
+++ day-5/app.js
@@ -213,19 +213,19 @@ console.log(updatedCart);
 
 /**Day 5: Exercises: Level 3
  
-1.Aşağıdaki dizide 10 öğrencinin yaşı vardır:
+1.The following is an array of 10 students ages:
 
 const  ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
-    .Diziyi sıralayın ve en küçük ve en büyük yaşı bulun
+    .Sort the array and find the min and max age
 
-    .Medyan yaşı bulun(dizinin ortasındaki eleman ama ortada iki eleman varsa elemanlar ikiye bölünür)
+    .Find the median age(one middle item or two middle items divided by two)
 
-    .Ortalama yaşı bulun(tüm elemanlar eleman sayısına bölünür)
+    .Find the average age(all items divided by number of items)
 
-    .Yaş aralığını bulun (maks - min)
+    .Find the range of the ages(max minus min)
 
-    .abs() metodunu kullanarak (min - ortalama) ve (maks - ortalama) değerlerini karşılaştırın,
+    .Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
 
 */  
 
@@ -233,7 +233,7 @@ const  ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
 
 /**
- Level: 2 Solutions
+ Level: 3 Solutions
 
  */
 
